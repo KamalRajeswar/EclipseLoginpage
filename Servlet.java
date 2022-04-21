282ddcf18b09b7702d158d54c77dacc535ee1037
KamalRajeswar <95246281+KamalRajeswar@users.noreply.github.com>
2022-04-21 14:58:22 +0530

    Add files via upload

---
+++ Servlet.java
@@ -0,0 +1,61 @@
+package temp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("username");  
+	    String p=request.getParameter("userpass");  
+	    
+	    System.out.println(n+p);      
+	    if(n.equals("user") && p.equals("user")){  
+	        RequestDispatcher rd=request.getRequestDispatcher("Servlet2");  
+	        rd.forward(request,response);  
+	    }  
+	    else{  
+	        out.print("Sorry username or password error");  
+//	        RequestDispatcher rd=request.getRequestDispatcher("index.html");  
+//	        rd.include(request,response);  
+	    }  
+	          
+	    out.close();  
+	}
+
+}
